520d2d8362d5f44dfa50e33d8228fc62bf06f089
Victor Gonzalez <vgonzal@ubuntu.(none)>
2012-04-25 02:16:34 -0300

    matMul con producto punto
    Signed-off-by: Victor Gonzalez <vgonzal@ubuntu.(none)>

--- CC1/Laboratorios/Laboratorio 1/1_benchmark.py
+++ CC1/Laboratorios/Laboratorio 1/1_benchmark.py
@@ -1,27 +1,52 @@
 import numpy, scipy, pylab
 import csv
+from time import time
 
 def LoadMatrix(path):
-""" Returns: 2D Array
+	""" Returns: 2D Array
 
-Funcion que lee archivos .csv y los retorna como una matriz 2D.
+	Funcion que lee archivos .csv y los retorna como una matriz 2D.
 
-"""
+	"""
 	# Leemos el archivo csv y preparamos la matriz de retorno
 	handle = csv.reader(open(path,"rb"),delimiter=",")
-	matrix = []
+	raw_matrix = []
 	
 	for line in handle:
 		# Una linea de csv lo transformamos en una fila de la matriz
 		row = list(line)
-		matrix.append(row)
+		row = [float(element) for element in row]
+		raw_matrix.append(row)
 	
-	return matrix
+	return raw_matrix
 	
 def MatrixMul(met,A,B):
-	print "Hello World"
+	start = 0.0
+	finish = 0.0
+	if met == "prod_punto":
+		print "Ejecutando producto punto..."
+		start = time()
+		result = numpy.dot(A,B)
+		finish = time()
+		print "Producto punto demoro " + str(finish - start) + " segundos."
+		print result
+	elif met == "enf_col":
+		print "Enfoque de Columna"
+	elif met == "enf_row":
+		print "Enfoque de Fila"
+	elif met == "enf_col_row":
+		print "Enfoque de Columna y Fila"
+	else:
+		print """[ERROR] Metodo desconocido.
+		Opciones: 'prod_punto', 'enf_col', 'enf_row', 'enf_col_row'.""" 
 	
 if __name__ == "__main__":
 	random = LoadMatrix("Anexos_lab1/random_matrix.csv")
 	vandermonde = LoadMatrix("Anexos_lab1/vandermonde.csv")
 	sparse = LoadMatrix("Anexos_lab1/sparse.csv")
+	test1 = LoadMatrix("Anexos_lab1/test1.csv")
+	test2 = LoadMatrix("Anexos_lab1/test2.csv")
+	
+	print test1
+	
+	MatrixMul("prod_punto", test1, test2)
