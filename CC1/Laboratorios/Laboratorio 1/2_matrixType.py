c46ed5d86ca98b86a63ab2439fd29d8611cf40bf
Victor Gonzalez <victor.gonzalezr@usm.cl>
2012-05-02 14:58:29 -0400

    new script for excercise 2
    Signed-off-by: Victor Gonzalez <victor.gonzalezr@usm.cl>

--- CC1/Laboratorios/Laboratorio 1/2_matrixType.py
+++ CC1/Laboratorios/Laboratorio 1/2_matrixType.py
@@ -2,6 +2,15 @@
 
 import numpy
 
+def squareMatrix(A):
+	n_rows = len(A)
+	n_cols = len(A[0])
+	
+	if n_rows == n_cols:
+		return True
+	else:
+		return False
+
 def transposeMatrix(A):
 	n_rows = len(A)
 	A_t = [None]*n_rows
@@ -10,18 +19,32 @@ def transposeMatrix(A):
 
 	for row_index in range(n_rows):
 		for col_index in range(n_rows):            
-			A_t[row_index][col_index] = A[row_index][col_index]	
+			A_t[row_index][col_index] = A[col_index][row_index]	
 	
 	return A_t
 
 def checkSimetry(A):
-	print "do nothing"
-
+	n_rows = len(A)
+	n_cols = len(A[0])
+	
+	# verify that the given matrix is a square matrix (nxn)
+	if squareMatrix(A) == False:
+		return ""
+	
+	# verify that the given matrix equals the transposed matrix
+	A_t = transposeMatrix(A)
+	for row_index in range(0,n_rows):
+		for col_index in range(0,n_cols):
+			if not A[row_index][col_index] == A_t[row_index][col_index]:
+				return ""
+	
+	# now that that we have an square matrix and the transposed matrix equals the original, anounce that tha matrix is symetrical.
+	return "simetrica"
+	
 if __name__ == "__main__":
 	simetrica = [[1,2,-3],[2,0,5],[-3,5,1]]
 	hermitica = [[1,2],[2,1]]
 	ortogonal = [[6,-2],[2,6]]
 	unitaria = [[4,-2],[-3,1]]
 	
-	print simetrica
-	print transposeMatrix(simetrica)
+	print checkSimetry(unitaria)
