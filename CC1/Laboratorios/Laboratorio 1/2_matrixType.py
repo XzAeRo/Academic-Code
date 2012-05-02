080fa9ed85ecbf41c9634fda646abf5f214cc8d1
Victor Gonzalez <victor.gonzalezr@usm.cl>
2012-05-02 16:58:58 -0400

    intentos no funcionales
    Signed-off-by: Victor Gonzalez <victor.gonzalezr@usm.cl>

--- CC1/Laboratorios/Laboratorio 1/2_matrixType.py
+++ CC1/Laboratorios/Laboratorio 1/2_matrixType.py
@@ -2,6 +2,7 @@
 
 import numpy
 
+
 def squareMatrix(A):
 	n_rows = len(A)
 	n_cols = len(A[0])
@@ -41,10 +42,37 @@ def checkSimetry(A):
 	# now that that we have an square matrix and the transposed matrix equals the original, anounce that tha matrix is symetrical.
 	return "simetrica"
 	
+def checkHermitian(A):
+	n_rows = len(A)
+	n_cols = len(A[0])
+	
+	# verify that the given matrix is a square matrix (nxn)
+	if squareMatrix(A) == False:
+		return ""
+	
+	# verify that the conjugate of the transposed matrix equals the matrix
+	A_t = transposeMatrix(A)
+	for row_index in range(0,n_rows):
+		for col_index in range(0,n_cols):
+			A_ij = complex(A[row_index][col_index])
+			A_t_ij = complex(A_t[row_index][col_index])
+			if A_ij != A_t_ij.conjugate():
+				return ""
+				
+	return "hermitiana"
+	
+def checkOrthogonal(A):
+	# do nothing
+	pass
+	
 if __name__ == "__main__":
 	simetrica = [[1,2,-3],[2,0,5],[-3,5,1]]
-	hermitica = [[1,2],[2,1]]
+	hermitica = [[3,'2+j'],['2-j',1]]
 	ortogonal = [[6,-2],[2,6]]
 	unitaria = [[4,-2],[-3,1]]
 	
-	print checkSimetry(unitaria)
+	A = [[1,2],[3,4]]
+	checkOrthogonal(A)
+	A = numpy.matrix(A)
+	print A[0]
+	#print checkSimetry(A), checkHermitian(A)
---
+++ CC1/Laboratorios/Laboratorio 1/3_image.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+import numpy, pylab
+
+if __name__ == "__main__":
+	A = imread("Anexos_lab1")
+	
+	imshow(A,origin="lower")
+	show()
