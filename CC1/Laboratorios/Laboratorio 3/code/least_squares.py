794f44f36497d22626360a2531c86dccd2b534f7
Victor Gonzalez <victor.gonzalezr@usm.cl>
2012-07-05 19:06:04 -0400

    pregunta 1 y pregunta 2
    Signed-off-by: Victor Gonzalez <victor.gonzalezr@usm.cl>

--- CC1/Laboratorios/Laboratorio 3/code/least_squares.py
+++ CC1/Laboratorios/Laboratorio 3/code/least_squares.py
@@ -1,13 +1,13 @@
 from numpy import *
 from pylab import *
+import random as rand
 
-
-def function(start=-10.0,end=10.0,step=0.01):
+def function(start=-5.0,end=5.0,step=0.01):
 	x = start
 	X = []
 	Y = []
 	while x <= end:
-		result = 2*sin(2*x) + 1.2*sin(5*x)
+		result = 2*sin(2*x) + 1.2*sin(5*x) + rand.uniform(0,1)
 		X.append(x)
 		Y.append(result)
 		x += step
@@ -15,8 +15,21 @@ def function(start=-10.0,end=10.0,step=0.01):
 	Y = array(Y)
 	return X, Y
 
+def pregunta1(x,y):
+	plot(x,y,'ro')
+	show()
+	
+def pregunta2(x,y):
+	degree1 = 7
+	degree2 = 20
+	p1 = poly1d(polyfit(x, y, degree1))
+	p2 = poly1d(polyfit(x, y, degree2))
+	xp = linspace(-5,5,100)
+	plot(x,y,".",xp,p1(xp),"-",xp,p2(xp),"--")
+	ylim(-5,5)
+	show()
+
 if __name__ == "__main__":
 	x,y = function()
-	plot(x,y,'ro')
-	plot(x+random(),y+random(),'bo')
-	show()
+	#pregunta1(x,y)
+	pregunta2(x,y)
