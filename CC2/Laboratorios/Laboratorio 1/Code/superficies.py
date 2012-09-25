a82f31c8bda0194b1fa0b429e696cecfed341e05
Victor Gonzalez <victor.gonzalezr@usm.cl>
2012-09-24 22:21:15 -0300

    I quitted and i'm no longer adding code to this
    Signed-off-by: Victor Gonzalez <victor.gonzalezr@usm.cl>

--- CC2/Laboratorios/Laboratorio 1/Code/superficies.py
+++ CC2/Laboratorios/Laboratorio 1/Code/superficies.py
@@ -11,26 +11,68 @@ except:
 import numpy as np
 import numpy.linalg as la
 
-def plot_figure(x,y,z):
-	fig = plt.figure()
-	ax = Axes3D(fig)
-	u = np.linspace(0, 2 * np.pi, 100)
-	v = np.linspace(0, np.pi, 100)
+def plot_implicit(fn, bbox=(-2.5,2.5)):
+    ''' create a plot of an implicit function
+    fn  ...implicit function (plot where fn==0)
+    bbox ..the x,y,and z limits of plotted interval'''
+    xmin, xmax, ymin, ymax, zmin, zmax = bbox*3
+    fig = plt.figure()
+    ax = Axes3D(fig)
+    A = np.linspace(xmin, xmax, 100) # resolution of the contour
+    B = np.linspace(xmin, xmax, 15) # number of slices
+    A1,A2 = np.meshgrid(A,A) # grid on which the contour is plotted
 
-	x = 10 * np.outer(np.cos(u), np.sin(v))
-	y = 10 * np.outer(np.sin(u), np.sin(v))
-	z = 10 * np.outer(np.ones(np.size(u)), np.cos(v))
-	ax.plot_surface(x, y, z,  rstride=4, cstride=4, color='b')
+    for z in B: # plot contours in the XY plane
+        X,Y = A1,A2
+        Z = fn(X,Y,z)
+        cset = ax.contour(X, Y, Z+z, [z], zdir='z')
+        # [z] defines the only level to plot for this contour for this value of z
 
-	plt.show()
+    for y in B: # plot contours in the XZ plane
+        X,Z = A1,A2
+        Y = fn(X,y,Z)
+        cset = ax.contour(X, Y+y, Z, [y], zdir='y')
 
-def eigen_surface(A,B,C):
-	Q, R = la.qr(A)
-	Q_inv = la.inv(Q)
-	D = Q_inv*A*Q
-	print D
-	print la.svd(A)[1]
+    for x in B: # plot contours in the YZ plane
+        Y,Z = A1,A2
+        X = fn(x,Y,Z)
+        cset = ax.contour(X+x, Y, Z, [x], zdir='x')
+
+    # must set plot limits because the contour will likely extend
+    # way beyond the displayed level.  Otherwise matplotlib extends the plot limits
+    # to encompass all values in the contour.
+    ax.set_zlim3d(zmin,zmax)
+    ax.set_xlim3d(xmin,xmax)
+    ax.set_ylim3d(ymin,ymax)
+
+    plt.show()
+
+def implicit_function(x,y,z):
+	xyz = np.transpose(np.array([x,y,z]))
+	return np.transpose(xyz)*D*xyz + B*Q
+def surface(A, B, C, D):
+	plot_implicit
+
+def eigen_surface(A, B, C):
+
+	A_k = A
+	Q_k = np.array
+	
+	for i in range(1,45):	
+		Q,R = la.qr(A_k)
+		A_k = np.dot(R,Q)
+		if i != 1:
+			Q_k=np.dot(Q_k,Q)
+		else: 
+			Q_k = Q
+			
+	D = np.dot(np.dot(la.inv(Q_k),A),Q_k)
+	
+	surface(A,B,C,D,Q_k)
+	
+	
 
 if __name__ == "__main__":
 	A = np.array([[5,2,0],[2,6,2],[0,2,7]])
-	eigen_surface(A,2,3)
+	B = np.array([0,0,0])
+	C = -1
