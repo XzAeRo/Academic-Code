b339d7d3e9588228295226e9d830725ae9203ab8
Victor Gonzalez <victor.gonzalezro@gmail.com>
2013-12-15 20:38:15 -0300

    start point
    Signed-off-by: Victor Gonzalez <victor.gonzalezro@gmail.com>

--- GraphicComputing/2013-t3-03.c
+++ GraphicComputing/2013-t3-03.c
@@ -17,18 +17,11 @@ Autores (Grupo 3):
 #include <stdio.h>
 #include <GL/glut.h>
 
-float x_translation;
-int toTheRight;
-float rotate_x;
-
 void init(){
     glClearColor(1.0,1.0,1.0,1.0);
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
     gluOrtho2D(-1.2,1.2,-1.2,1.2);
-    x_translation = 0.0;
-    toTheRight = 1;
-    rotate_x = 0.0;
 }
 
 void dibujarFiguras(void) {
@@ -90,59 +83,6 @@ void cambiarDimension(int w, int h) {
     printf("[display] Nueva dimension de ventana:\n\t\tAncho: %i px\n\t\tAlto: %i px\n",w,h);
 }
 
-void animarEscena(){
-    glClear(GL_COLOR_BUFFER_BIT);
-
-    // triangulo
-    glTranslatef(x_translation ,0.0f,0.0f); // trasladamos en el eje x
-    glBegin(GL_POLYGON);
-        glColor3f(1.0,0.0,0.0);
-        glVertex2f(-0.5,0.0);
-        glVertex2f(-0.0,-1.0);
-        glVertex2f(-1.0,-1.0);
-    glEnd();
-    glLoadIdentity();
-
-    /* Si el dibujo se sale de la pantalla, hacer que se devuelva */
-    if (toTheRight == 1){    
-        x_translation += 0.01;
-        if (x_translation > 2.3)
-            toTheRight = 0;
-    } else {
-        x_translation -= 0.01;
-        if (x_translation < -1.3)
-            toTheRight = 1;
-    }
-    
-    //Cuadrado
-    /* Notar que OpenGl aplica las transformaciones en el orden
-    inverso en el aparecen */
-    glTranslatef(0.5f,-0.5f,0.0f); // 3.- devolvemos al sitio original
-    glRotatef(rotate_x,0.0f,0.0f,1.0f); // 2.- hacemos rotar en torno al eje z
-    glTranslatef(-0.5f,0.5f,0.0f); // 1.- primero trasladamos al centro
-    glBegin(GL_POLYGON);
-        glColor3f(0.0,1.0,0.0);
-        glVertex2f(0.0,-1.0);
-        glVertex2f(1.0,-1.0);
-        glVertex2f(1.0,0.0);
-        glVertex2f(0.0,0.0);
-    glEnd();
-    glLoadIdentity();
-
-    rotate_x += 5;
-
-    //Rectangulo
-    glBegin(GL_POLYGON);                                                         
-        glColor3f(0.0,0.0,1.0);
-        glVertex2f(-1.0,0.1);                          
-        glVertex2f(-1.0,1.0);                            
-        glVertex2f(1.0,1.0);                             
-        glVertex2f(1.0,0.1);                                             
-    glEnd();
-
-    glutSwapBuffers();
-}
-
 
 int main(int argc, char **argv) {
 
@@ -166,9 +106,6 @@ int main(int argc, char **argv) {
     printf("[display] Redimensionado de ventana... ");
     glutReshapeFunc(cambiarDimension);
     printf("Listo\n");
-
-    // callback
-    glutIdleFunc(animarEscena);
 
     // inicializar el pintor
     glutMainLoop();
